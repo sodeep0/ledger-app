eeeb0a392db6b981205464bc248dba4c575183a3
sudip0-0 <sudipshrestha196@gmail.com>
2025-09-05 18:27:34 +0545

    auth env

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,7 +1,7 @@
 // src/services/authService.js
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5001/api/users/';
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api/users/';
 
 // Register user
 const register = async (userData) => {
